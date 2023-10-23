5e07f3d4bf5354d353e1767d82d60e50e5d6fff5
Тетяна Невідома <nevidomatanya89@gmail.com>
2023-10-23 12:22:34 +0300

    upd

---
+++ README 2.md
@@ -0,0 +1,1 @@
+srcset="./images/mykola.jpg 1x, ./images/mykola-2x.jpg 2x"
--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,14 +1,14 @@
 (() => {
-    const refs = {
-        openModalBtn: document.querySelector('[data-nav-open]'),
-        closeModalBtn: document.querySelector('[data-nav-close]'),
-        modal: document.querySelector('[data-nav]'),
-    };
+  const refs = {
+    openMenuBtn: document.querySelector('[data-menu-open]'),
+    closeMenuBtn: document.querySelector('[data-menu-close]'),
+    menu: document.querySelector('[data-menu]'),
+  };
 
-    refs.openModalBtn.addEventListener('click', toggleModal);
-    refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
 
-    function toggleModal() {
-        refs.modal.classList.toggle('is-hidden--mob');
-    }
+  function toggleMenu() {
+    refs.menu.classList.toggle('is-hidden');
+  }
 })();
